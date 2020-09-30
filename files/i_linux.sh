95c9a2a97bad656f7996345fc65f4997c244f1da
David <davidmumar@gmail.com>
2020-09-30 02:05:14 +0200

    Add files via upload

--- files/i_linux.sh
+++ files/i_linux.sh
@@ -17,6 +17,7 @@ PARAR_TVHEADEND="sudo service tvheadend stop"
 INICIAR_TVHEADEND="sudo service tvheadend start"
 
 NOMBRE_APP="dobleM"
+NOMBRE_APP_IPTV="dobleM-IPTV"
 CARPETA_DOBLEM="$CARPETA_TVH/dobleM"
 CARPETA_SCRIPT="$PWD"
 
@@ -41,6 +42,9 @@ INFO_CARPETA_GRABBER="$(which tvheadend | sed 's/\/tvheadend//')"
 ver_local=`cat $CARPETA_TVH/dobleM.ver 2>/dev/null`
 ver_web=`curl https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM.ver 2>/dev/null`
 
+ver_local_IPTV=`cat $CARPETA_TVH/dobleM-IPTV.ver 2>/dev/null`
+ver_web_IPTV=`curl https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM-IPTV.ver 2>/dev/null`
+
 clear
 
 # Copia de seguridad
@@ -62,10 +66,10 @@ backup()
 	echo "$magenta 2. Realizando copia de seguridad $end"	
 	if [ -f "$CARPETA_SCRIPT/Backup_Tvheadend_$(date +"%Y-%m-%d").tar.xz" ]; then
 		FILE="Backup_Tvheadend_$(date +"%Y-%m-%d__%H-%M-%S").tar.xz"
-		tar -cJf $CARPETA_SCRIPT/$FILE bouquet channel epggrab input/dvb picons 
+		tar -cJf $CARPETA_SCRIPT/$FILE bouquet channel epggrab input/dvb input/iptv picons 
 	else
 		FILE="Backup_Tvheadend_$(date +"%Y-%m-%d").tar.xz"
-		tar -cJf $CARPETA_SCRIPT/$FILE bouquet channel epggrab input/dvb picons 
+		tar -cJf $CARPETA_SCRIPT/$FILE bouquet channel epggrab input/dvb input/iptv picons 
 	fi
 	
 # Reiniciamos el servicio de tvheadend
@@ -84,7 +88,7 @@ backup()
 		read CAD
 }
 
-# Instalador
+# INSTALADOR SATELITE
 install()
 {
 	echo
@@ -150,11 +154,12 @@ install()
 	echo			
 	echo "$magenta 5. Instalando grabber $end"
 		rm -f $CARPETA_GRABBER/tv_grab_EPG_dobleM
-		rm -rf $CARPETA_TVH/epggrab/xmltv/
-		
-		cp -r $CARPETA_DOBLEM/epggrab/ $CARPETA_TVH
+		rm -f $CARPETA_GRABBER/tv_grab_EPG_dobleM-IPTV
 		cp -r $CARPETA_DOBLEM/grabber/tv_grab_EPG_dobleM $CARPETA_GRABBER
+		cp -r $CARPETA_DOBLEM/grabber/tv_grab_EPG_dobleM-IPTV $CARPETA_GRABBER
 		
+		rm -rf $CARPETA_TVH/epggrab/xmltv/
+		cp -r $CARPETA_DOBLEM/epggrab/ $CARPETA_TVH
 		sed -i -- "s,\"modid\":.*,\"modid\": \"$CARPETA_GRABBER/tv_grab_EPG_dobleM\"\,,g" $CARPETA_TVH/epggrab/xmltv/channels/*
 	
 while :	
@@ -171,11 +176,13 @@ do
 	esac
 done
 		
+		chown -R $USER_TVH:$GROUP_TVH $CARPETA_GRABBER/tv_grab_EPG_dobleM
+		chown -R $USER_TVH:$GROUP_TVH $CARPETA_GRABBER/tv_grab_EPG_dobleM-IPTV
+		chmod +rx $CARPETA_GRABBER/tv_grab_EPG_dobleM
+		chmod +rx $CARPETA_GRABBER/tv_grab_EPG_dobleM-IPTV
+		
 		chown -R $USER_TVH:$GROUP_TVH $CARPETA_TVH/epggrab/
 		chmod -R $TVHEADEND_EPGGRAB_PERMISSIONS $CARPETA_TVH/epggrab/
-		
-		chown -R $USER_TVH:$GROUP_TVH $CARPETA_GRABBER/tv_grab_EPG_dobleM
-		chmod +rx $CARPETA_GRABBER/tv_grab_EPG_dobleM
 
 # Damos permisos a los directorios
 	echo
@@ -207,7 +214,8 @@ done
 		sed -i 's/"cron": .*,/"cron": "# Se ejecuta todos los días a las 8:10\\n10 8 * * *",/g' $CARPETA_TVH/epggrab/config
 		sed -i 's/"enabled": .*,/"enabled": false,/g' $CARPETA_TVH/epggrab/config
 		sed -i '/tv_grab_EPG_dobleM/,/},/d' $CARPETA_TVH/epggrab/config
-		sed -i 's/"modules": {/"modules": {\n\t\t"CARPETA_GRABBER\/tv_grab_EPG_dobleM": {\n\t\t\t"class": "epggrab_mod_int_xmltv",\n\t\t\t"dn_chnum": 0,\n\t\t\t"name": "XMLTV: EPG_dobleM - Movistar+",\n\t\t\t"type": "Internal",\n\t\t\t"enabled": true,\n\t\t\t"priority": 3\n\t\t},/g' $CARPETA_TVH/epggrab/config
+		sed -i '/tv_grab_EPG_dobleM-IPTV/,/},/d' $CARPETA_TVH/epggrab/config
+		sed -i 's/"modules": {/"modules": {\n\t\t"CARPETA_GRABBER\/tv_grab_EPG_dobleM": {\n\t\t\t"class": "epggrab_mod_int_xmltv",\n\t\t\t"dn_chnum": 0,\n\t\t\t"name": "XMLTV: EPG_dobleM - Movistar+",\n\t\t\t"type": "Internal",\n\t\t\t"enabled": true,\n\t\t\t"priority": 3\n\t\t},\n\t\t"CARPETA_GRABBER\/tv_grab_EPG_dobleM-IPTV": {\n\t\t\t"class": "epggrab_mod_int_xmltv",\n\t\t\t"dn_chnum": 0,\n\t\t\t"name": "XMLTV: EPG_dobleM - IPTV",\n\t\t\t"type": "Internal",\n\t\t\t"enabled": true,\n\t\t\t"priority": 3\n\t\t},/g' $CARPETA_TVH/epggrab/config
 		sed -i "s,CARPETA_GRABBER,$CARPETA_GRABBER,g" $CARPETA_TVH/epggrab/config
 		
 # Borramos carpeta termporal dobleM
@@ -238,10 +246,152 @@ done
 	echo "$blue ################################################################# $end"
 	echo "$blue ###                  Gracias por usar dobleM                  ### $end" 
 	echo "$blue ################################################################# $end" 
-	echo 
-		rm -rf $CARPETA_SCRIPT/i_*.sh
+		echo
+		echo "$green Pulsa intro para continuar... $end"
+		read CAD
 }
 
+# INSTALADOR IPTV
+installIPTV()
+{
+	echo
+	echo "$blue ################################################################ $end"
+	echo "$blue ###  Iniciando instalación de lista de canales y EPG dobleM  ### $end" 
+	echo "$blue ################################################################ $end" 	
+
+# Paramos tvheadend para evitar conflictos al copiar y/o borrar archivos	
+	echo
+	echo "$magenta 1. Parando servicio tvheadend $end"
+		$PARAR_TVHEADEND
+		cd $CARPETA_TVH
+				
+# Borramos carpeta dobleM e input actual de tvheadend
+	echo
+	echo "$magenta 2. Borrando instalación anterior $end"
+	rm -rf $CARPETA_DOBLEM
+	rm -rf $CARPETA_TVH/input/iptv/networks/d040f9ac2f2bfe2df2af82722cf1a7b6/
+	
+# Borramos channels y tags marcados, conservando redes y canales mapeados por los usuarios
+	rm -f 
+		if [ "$1" != "ALL" ];then
+			# Recorremos los ficheros de estas carpetas para borrar solo los que tengan la marca dobleM
+			for fichero in $CARPETA_TVH/channel/config/* $CARPETA_TVH/channel/tag/*
+			do 
+				if [ -f "$fichero" ]; then  
+					ultima=$(tail -n 1 $fichero)
+					if [ "$ultima" = $NOMBRE_APP_IPTV ]; then
+					rm -f $fichero 
+					fi
+				fi
+			done
+		else
+			# Borramos todos los canales y tags
+			rm -rf $CARPETA_TVH/channel/
+	fi		
+
+# Descargamos el tar de dobleM y lo descomprimimos en CARPETA_DOBLEM		
+	echo
+	echo "$magenta 3. Descargando nueva lista de canales $end" 		
+		mkdir $CARPETA_DOBLEM
+		cd $CARPETA_DOBLEM
+		wget -q https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM-IPTV.tar.xz
+		tar -xf "dobleM-IPTV.tar.xz"
+	# Marcamos con dobleM al final todos los archivos de la carpeta /channel/config/ y /channel/tag/
+		sed -i '/^\}$/,$d' $CARPETA_DOBLEM/channel/config/*
+		sed -i '/^\}$/,$d' $CARPETA_DOBLEM/channel/tag/*
+		sed -i "\$a}\n$NOMBRE_APP_IPTV" $CARPETA_DOBLEM/channel/config/*
+		sed -i "\$a}\n$NOMBRE_APP_IPTV" $CARPETA_DOBLEM/channel/tag/*
+
+# Empezamos a copiar los archivos necesarios
+	echo	
+	echo "$magenta 4. Instalando lista de canales $end"
+		cp -r $CARPETA_DOBLEM/dobleM-IPTV.ver $CARPETA_TVH
+		cp -r $CARPETA_DOBLEM/channel/ $CARPETA_TVH
+		cp -r $CARPETA_DOBLEM/input/ $CARPETA_TVH
+
+# Instalación de grabbers. Borramos grabbers viejos y copiamos grabbers nuevos. Damos permisos.
+	echo			
+	echo "$magenta 5. Instalando grabber $end"
+		rm -f $CARPETA_GRABBER/tv_grab_EPG_dobleM
+		rm -f $CARPETA_GRABBER/tv_grab_EPG_dobleM-IPTV		
+		cp -r $CARPETA_DOBLEM/grabber/tv_grab_EPG_dobleM $CARPETA_GRABBER
+		cp -r $CARPETA_DOBLEM/grabber/tv_grab_EPG_dobleM-IPTV $CARPETA_GRABBER
+		
+while :	
+do
+	echo "   5a. Escoge que tipo de imágenes quieres que aparezcan en la guía:"
+	echo "	1) Posters"
+	echo "	2) Fanarts"
+	echo -n "	Indica una opción: "
+	read opcion
+	case $opcion in
+			1) sed -i 's/enable_fanart=.*/enable_fanart=false/g' $CARPETA_GRABBER/tv_grab_EPG_dobleM; break;;
+			2) sed -i 's/enable_fanart=.*/enable_fanart=true/g' $CARPETA_GRABBER/tv_grab_EPG_dobleM; break;;	
+			*) echo "$opcion es una opción inválida";
+	esac
+done
+		
+		chown -R $USER_TVH:$GROUP_TVH $CARPETA_GRABBER/tv_grab_EPG_dobleM
+		chown -R $USER_TVH:$GROUP_TVH $CARPETA_GRABBER/tv_grab_EPG_dobleM-IPTV
+		chmod +rx $CARPETA_GRABBER/tv_grab_EPG_dobleM
+		chmod +rx $CARPETA_GRABBER/tv_grab_EPG_dobleM-IPTV
+
+# Damos permisos a los directorios
+	echo
+	echo "$magenta 6. Aplicando permisos $end"
+		chown -R $USER_TVH:$GROUP_TVH $CARPETA_TVH/channel/
+		chown -R $USER_TVH:$GROUP_TVH $CARPETA_TVH/input/
+		
+		chmod -R $TVHEADEND_CHANNEL_PERMISSIONS $CARPETA_TVH/channel/
+		chmod -R $TVHEADEND_INPUT_PERMISSIONS $CARPETA_TVH/input/
+	
+# Configuramos tvheadend
+	echo
+	echo "$magenta 7. Configurando tvheadend $end"
+		#Idiomas EPG config tvheadend
+		sed -i 's/"language": \[/"language": \[\ndobleM/g' $CARPETA_TVH/config
+		sed -i '/dobleM/,/],/d' $CARPETA_TVH/config
+		sed -i 's/"language": \[/"language": \[\n\t\t"spa",\n\t\t"eng",\n\t\t"ger",\n\t\t"fre"\n\t\],/g' $CARPETA_TVH/config
+		#picons config tvheadend
+		sed -i '/"chiconscheme": .*,/d' $CARPETA_TVH/config
+		sed -i '/"piconpath": .*,/d' $CARPETA_TVH/config
+		sed -i '/"piconscheme": .*,/d' $CARPETA_TVH/config
+		sed -i 's/"prefer_picon": .*,/"prefer_picon": false,\n\t"chiconscheme": 2,\n\t"piconpath": "file:\/\/CARPETA_TVH\/picons",\n\t"piconscheme": 0,/g' $CARPETA_TVH/config
+		sed -i "s,CARPETA_TVH,$CARPETA_TVH,g" $CARPETA_TVH/config
+		#cron y grabber config epggrab
+		sed -i 's/"cron": .*,/"cron": "# Se ejecuta todos los días a las 8:10\\n10 8 * * *",/g' $CARPETA_TVH/epggrab/config
+		sed -i 's/"enabled": .*,/"enabled": false,/g' $CARPETA_TVH/epggrab/config
+		sed -i '/tv_grab_EPG_dobleM/,/},/d' $CARPETA_TVH/epggrab/config
+		sed -i '/tv_grab_EPG_dobleM-IPTV/,/},/d' $CARPETA_TVH/epggrab/config
+		sed -i 's/"modules": {/"modules": {\n\t\t"CARPETA_GRABBER\/tv_grab_EPG_dobleM": {\n\t\t\t"class": "epggrab_mod_int_xmltv",\n\t\t\t"dn_chnum": 0,\n\t\t\t"name": "XMLTV: EPG_dobleM - Movistar+",\n\t\t\t"type": "Internal",\n\t\t\t"enabled": true,\n\t\t\t"priority": 3\n\t\t},\n\t\t"CARPETA_GRABBER\/tv_grab_EPG_dobleM-IPTV": {\n\t\t\t"class": "epggrab_mod_int_xmltv",\n\t\t\t"dn_chnum": 0,\n\t\t\t"name": "XMLTV: EPG_dobleM - IPTV",\n\t\t\t"type": "Internal",\n\t\t\t"enabled": true,\n\t\t\t"priority": 3\n\t\t},/g' $CARPETA_TVH/epggrab/config
+		sed -i "s,CARPETA_GRABBER,$CARPETA_GRABBER,g" $CARPETA_TVH/epggrab/config
+		
+# Borramos carpeta termporal dobleM
+	echo
+	echo "$magenta 8. Eliminando archivos temporales $end"
+		rm -rf $CARPETA_DOBLEM
+	
+# Reiniciamos el servicio de tvheadend
+	echo
+	echo "$magenta 9. Iniciando servicio tvheadend $end"
+		cd $CARPETA_SCRIPT
+		$INICIAR_TVHEADEND
+
+# Fin instalación
+	echo 
+	echo " La primera captura de EPG tardará unos minutos hasta que todos"
+	echo " los procesos de tvheadend se terminen de iniciar, ten paciencia."
+	echo 
+	echo "$blue ################################################################# $end"
+	echo "$blue ###                  Gracias por usar dobleM                  ### $end" 
+	echo "$blue ################################################################# $end" 
+	echo 
+		echo
+		echo "$green Pulsa intro para continuar... $end"
+		read CAD
+}
+
+# LIMPIEZA TOTAL DE CANALES
 limpiezatotalcanales()
 {
 	echo
@@ -296,28 +446,32 @@ do
 	echo " Directorio instalación grabber:  $green $CARPETA_GRABBER $end"
 	echo
 	echo " Vas a ejecutar el script:$green $NOMBRE_SCRIPT $end"
-	echo " Versión instalada:$red $ver_local $end --->  Nueva versión:$green $ver_web $end"
+	echo " Versión SATELITE instalada:$red $ver_local $end --->  Nueva versión:$green $ver_web $end"
+	echo " Versión IPTV instalada:$red $ver_local_IPTV $end --->  Nueva versión:$green $ver_web_IPTV $end"
 	echo
 	echo "------------------------------------------------------------------"
 	echo
 	echo " 1)$green Hacer copia de seguridad de tvheadend $end"
 	echo
-	echo " 2)$cyan Instalar lista de canales, picons, grabber y configurar tvheadend $end"
+	echo " 2)$cyan Instalar lista de canales$yellow SATELITE $end+ picons, grabber y configurar tvheadend $end"
+	echo
+	echo " 3)$cyan Instalar lista de canales$yellow IPTV $end+ grabber y configurar tvheadend $end"
 	echo 
-	echo " 3)$blue Hacer una limpieza total de canales $end"
+	echo " 4)$blue Hacer una limpieza total de canales $end"
 	echo 
-    echo " 4)$magenta Volver $end"
+    echo " 5)$magenta Volver $end"
 	echo 
-    echo " 5)$red Salir $end"
+    echo " 6)$red Salir $end"
 	echo
 	echo -n " Indica una opción: "
 	read opcion
 	case $opcion in
-		1) backup && clear;;
-		2) install; break;;
-		3) limpiezatotalcanales && clear;;
-		4) rm -rf $CARPETA_SCRIPT/$NOMBRE_SCRIPT && clear && sudo sh $CARPETA_SCRIPT/i_dobleM.sh; break;;
-		5) rm -rf $CARPETA_SCRIPT/i_*.sh; exit;;		
+		1) clear && backup;;
+		2) clear && install;;
+		3) clear && installIPTV;;
+		4) clear && limpiezatotalcanales;;
+		5) rm -rf $CARPETA_SCRIPT/$NOMBRE_SCRIPT && clear && sudo sh $CARPETA_SCRIPT/i_dobleM.sh; break;;
+		6) rm -rf $CARPETA_SCRIPT/i_*.sh; exit;;		
 		*) echo "$opcion es una opción inválida\n";
 	esac
 done
