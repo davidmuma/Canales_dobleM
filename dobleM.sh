7defb1f9ecd54eea2c6775aee8a8331dd7c1203d
David <davidmumar@gmail.com>
2020-09-30 16:23:46 +0200

    Add files via upload

--- dobleM.sh
+++ dobleM.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 #### Script creado por dobleM
 
-# Comprobamos si se está ejecutando como root
-if [ $(id -u) -ne 0 ]; then
-	printf "\e[31mERROR:\e[0m Por favor, ejecuta el script como root.
-	
-	Puedes hacerlo de diferentes formas:
-	- Mediante el comando \"sudo sh $0\"
-	- Entrando en la sesión del propio root con \"sudo -i\"
-	  y después ejecutando el script con \"sh $0\"\n\n"
-	exit 1
-fi
-
 echo
 echo Descargando última versión del instalador...
 sleep 1
@@ -21,4 +10,4 @@ echo
 sleep 1
 echo Ejecutando instalador...
 sleep 1
-sudo sh i_dobleM.sh
+sh i_dobleM.sh
