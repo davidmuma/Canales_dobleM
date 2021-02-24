5855abd03f697f22af6484475a61fad02e3c2962
David <davidmumar@gmail.com>
2021-02-24 16:30:06 +0100

    Add files via upload

--- files/i_dobleMe.sh
+++ files/i_dobleMe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# - Script creado por dobleM
+# - script creado por dobleM
 #Formatear texto con colores: https://unix.stackexchange.com/a/92568
 red='\e[1;31m'
 green='\e[1;32m'
@@ -8,13 +8,14 @@ blue='\e[1;34m'
 magenta='\e[1;35m'
 cyan='\e[1;36m'
 end='\e[0m'
-#curl -s -O https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMe.sh
 
 CARPETA_SCRIPT="$PWD"
 
-	clear
 	echo -e "$blue ############################################################################# $end"
-	echo -e "$blue ###                        Instalador para enigma2                        ### $end"
+	echo -e "$blue ###                           $green -= dobleM =- $end                             $blue ### $end"
+	echo -e "$blue ###                     Telegram: $cyan t.me/EPG_dobleM $end                      $blue ### $end"
+	echo -e "$blue ### --------------------------------------------------------------------- ### $end"
+	echo -e "$blue ###                        Instalador para Enigma2                        ### $end"
 	echo -e "$blue ############################################################################# $end"
 	echo
 	while :
@@ -25,7 +26,7 @@ CARPETA_SCRIPT="$PWD"
 		read opcionEPG
 		case $opcionEPG in
 				s) clear && enigma2_EPG;;
-				n) rm -rf $CARPETA_SCRIPT/i_dobleM*.sh; exit;;
+				n) clear && echo " Gracias por usar el script dobleM" && rm -rf $CARPETA_SCRIPT/i_dobleM*.sh; exit;;
 				*) echo && echo " Por favor, elige Si o No" && echo;
 		esac
 	done
@@ -37,7 +38,7 @@ enigma2_EPG()
    echo "No tiene instalado epg import en su receptor, realice la instalacion y vuelva a intentarlo"
    sleep 5
  else
-   curl -s -O http://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM_e2EPG.sources.tar
+   curl -sO https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM_e2EPG.sources.tar
    tar xvf dobleM_e2EPG.sources.tar -C /etc/epgimport/
    rm -r dobleM_e2EPG.sources.tar
    echo "Ha finalizado la instalacion de EPG dobleM, espere unos segundos y volverá al menu"
