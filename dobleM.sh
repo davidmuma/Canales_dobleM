d6df70fac1ef4f19ec1683267de92502d6fb335d
David <davidmumar@gmail.com>
2021-02-26 09:24:54 +0100

    Update dobleM.sh

--- dobleM.sh
+++ dobleM.sh
@@ -5,10 +5,20 @@ echo
 echo Descargando última versión del instalador...
 sleep 1
 echo
-curl -# -O https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleM.sh
-echo
-sleep 1
-echo Ejecutando instalador...
-sleep 1
-clear
-sh i_dobleM.sh
+if [ -d /etc/enigma2 ]; then
+	curl -# -O https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMe.sh
+	echo
+	sleep 1
+	echo Ejecutando instalador para enigma2...
+	sleep 1
+	clear
+	sh i_dobleMe.sh
+else
+	curl -# -O https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleM.sh
+	echo
+	sleep 1
+	echo Ejecutando instalador para tvheadend...
+	sleep 1
+	clear
+	sh i_dobleM.sh
+fi
