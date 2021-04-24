bf9803b792e411c3473286348c27a6376c5500e7
David <davidmumar@gmail.com>
2021-04-25 01:47:20 +0200

    Add files via upload

--- files/i_dobleMe.sh
+++ files/i_dobleMe.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 # - script creado por dobleM
-#Formatear texto con colores: https://unix.stackexchange.com/a/92568
-red='\e[1;31m'
-green='\e[1;32m'
-yellow='\e[1;33m'
-blue='\e[1;34m'
-magenta='\e[1;35m'
-cyan='\e[1;36m'
-end='\e[0m'
 
 CARPETA_SCRIPT="$PWD"
+CARPETA_SKIN="/usr/share/enigma2"
+#CARPETA_SKIN="/mnt/c/pru/skin"
 
 clear
 echo Cargando...
@@ -26,52 +20,47 @@ instalarEPG()
 {
 	cd $CARPETA_SCRIPT
 	if [ ! -d /etc/epgimport/ ]; then
-		echo "$red No tienes instalado EPG-Import en tu receptor, $end"
-		echo "$red realiza la instalación y vuelve a intentarlo $end"
+		echo " No tienes instalado EPG-Import en tu receptor,"
+		echo " realiza la instalación y vuelve a intentarlo"
 		echo
 		echo " Pulsa intro para continuar..."
 		read CAD
 		MENU
 	else
 		clear
-		echo -e "$blue ############################################################ $end"
-		echo -e "$blue ###       $green Comienza la instalación de los sources       $blue ### $end"
-		echo -e "$blue ############################################################ $end"
+		echo " ############################################################"
+		echo " ###        Comienza la instalación de los sources        ###"
+		echo " ############################################################"
 		echo
-		printf "%-$(($COLUMNS-10))s"  " 1. Probando descarga con curl"
 			ERROR=false
-			curl -sO https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM_E2.sources.tar 2>>$CARPETA_SCRIPT/dobleM.log
-			if [ $? -eq 0 ]; then
-				printf "%s$green%s$end%s\n" "[" "  OK  " "]"
-			else
-				printf "%s$red%s$end%s\n" "[" "FAILED" "]"
-			fi
-		printf "%-$(($COLUMNS-10))s"  " 2. Probando descarga con wget"
+		printf "%-$(($COLUMNS-10))s"  " 1. Descargando sources"
 			wget -O dobleM_E2.sources.tar https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/dobleM_E2.sources.tar 2>>$CARPETA_SCRIPT/dobleM.log
 			if [ $? -eq 0 ]; then
-				printf "%s$green%s$end%s\n" "[" "  OK  " "]"
+				printf "%s%s%s\n" "[" "  OK  " "]"
 			else
-				printf "%s$red%s$end%s\n" "[" "FAILED" "]"
-			fi
-		printf "%-$(($COLUMNS-10))s"  " 3. Copiando sources"
-			tar xf dobleM_E2.sources.tar -C /etc/epgimport/ 2>/dev/null 2>>$CARPETA_SCRIPT/dobleM.log
-			if [ $? -eq 0 ]; then
-				printf "%s$green%s$end%s\n" "[" "  OK  " "]"
-			else
-				printf "%s$red%s$end%s\n" "[" "FAILED" "]"
-			fi
-		printf "%-$(($COLUMNS-10))s"  " 4. Borrando temporales"
-			rm -r dobleM_E2.sources.tar 2>>$CARPETA_SCRIPT/dobleM.log
-			if [ $? -eq 0 ]; then
-				printf "%s$green%s$end%s\n" "[" "  OK  " "]"
-			else
-				printf "%s$red%s$end%s\n" "[" "FAILED" "]"
-				echo -e "\n La descarga ha fallado.\n Por favor, inténtalo más tarde."
+				printf "%s%s%s\n" "[" "FAILED" "]"
+				echo
+				echo " La descarga ha fallado"
+				echo " Por favor, inténtalo más tarde"
 				echo
 				echo " Pulsa intro para continuar..."
 				read CAD
 				MENU
 			fi
+		printf "%-$(($COLUMNS-10))s"  " 2. Copiando sources"
+			tar xf dobleM_E2.sources.tar -C /etc/epgimport/ 2>/dev/null 2>>$CARPETA_SCRIPT/dobleM.log
+			if [ $? -eq 0 ]; then
+				printf "%s%s%s\n" "[" "  OK  " "]"
+			else
+				printf "%s%s%s\n" "[" "FAILED" "]"
+			fi
+		printf "%-$(($COLUMNS-10))s"  " 3. Borrando temporales"
+			rm -r dobleM_E2.sources.tar 2>>$CARPETA_SCRIPT/dobleM.log
+			if [ $? -eq 0 ]; then
+				printf "%s%s%s\n" "[" "  OK  " "]"
+			else
+				printf "%s%s%s\n" "[" "FAILED" "]"
+			fi			
 		echo
 		echo " Ha finalizado la instalación, ves a EPG-Import y pulsa el boton Azul (Fuentes)"
 		echo " Elige la EPG dobleM que mas te guste y luego pulsa el boton Verde (Guardar)"
@@ -91,66 +80,74 @@ instalarEPG()
 elegirSKIN()
 {
 	cd $CARPETA_SCRIPT
+	find $CARPETA_SKIN -name skin.xml | sed -e 's#/skin.xml##' > i_dobleMskin.sh
+	LISTADO_SKINS="$(find $CARPETA_SKIN -name skin.xml | nl -s ") " | sed -e 's#/skin.xml##' -e "s#$CARPETA_SKIN/##" -e 's#    ##')"
 	clear
-	echo -e "$blue ############################################################ $end"
-	echo -e "$blue ###           $cyan Selecciona el skin a modificar           $blue ### $end"
-	echo -e "$blue ############################################################ $end"
+	echo " ############################################################"
+	echo " ###            Selecciona el skin a modificar            ###"
+	echo " ############################################################"
 	echo
 	while :
 	do
-		echo -e " a) PLi-HD"
-		echo -e " b) OpenStarHD"
-		echo -e " c) OctEtFHD"
-		echo -e " d) DarknessFHD"
-		echo -e " e) MetrixHD"
-		echo -e " f) AtileHD"
+		echo "$LISTADO_SKINS"
 		echo
-		echo -e " 0)$magenta Volver al menú$end"
+		echo " v) Volver al menú"
 		echo
-		echo -e " 1)$green Introducir ruta manualmente $end"
+		echo " m) Introducir ruta manualmente"
 		echo
 		echo -n " Indica una opción: "
 		read opcionskin
 		case $opcionskin in
-				a) RUTASKIN=/usr/share/enigma2/PLi-HD; break;;
-				b) RUTASKIN=/usr/share/enigma2/OpenStarHD; break;;
-				c) RUTASKIN=/usr/share/enigma2/OctEtFHD; break;;
-				d) RUTASKIN=/usr/share/enigma2/DarknessFHD; break;;
-				e) RUTASKIN=/usr/share/enigma2/MetrixHD; break;;
-				f) RUTASKIN=/usr/share/enigma2/AtileHD; break;;
-				0) MENU;;
-				1)  clear
+				1) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				2) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				3) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				4) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				5) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				6) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				7) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				8) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				9) RUTASKIN="$(sed -n "$opcionskin p" i_dobleMskin.sh)"; break;;
+				v) MENU;;
+				m)  clear
 					echo
-					echo -e "Introduce la ruta de tu skin y pulsa INTRO "
+					echo "Introduce la ruta de tu skin y pulsa INTRO "
 					echo
 					read RUTASKIN; break;;
 				*) echo && echo " $opcionskin es una opción inválida" && echo;
 		esac
 	done
-			if [ "$VAR" = "restaurar" ]; then
-				echo -e " MOD"
-				restaurarSKIN
-			else
-				echo -e " RES"
-				modificarSKIN
-			fi
+		if [ ! -f $RUTASKIN/skin.xml ]; then
+			echo " No existe el skin que has seleccionado,"
+			echo " comprueba la ruta y vuelve a intentarlo"
+			echo
+			echo " Pulsa intro para continuar..."
+			read CAD
+			MENU
+		fi
+		if [ "$VAR" = "restaurar" ]; then
+			echo " MOD"
+			restaurarSKIN
+		else
+			echo " RES"
+			modificarSKIN
+		fi
 }
 
 modificarSKIN()
 {
 	cd $CARPETA_SCRIPT
 	clear
-	echo -e "$blue ############################################################ $end"
-	echo -e "$blue ###         $cyan Selecciona el tipo de letra a usar         $blue ### $end"
-	echo -e "$blue ############################################################ $end"
+	echo " ############################################################"
+	echo " ###          Selecciona el tipo de letra a usar          ###"
+	echo " ############################################################"
 	echo
 	while :
 	do
-		echo -e " 1) NanumGothic"
-		echo -e " 2) RocknRollOne"
-		echo -e " 3) Rounded"
-		echo -e " 4) SawarabiGothic"
-		echo -e " 5) Titre"
+		echo " 1) NanumGothic"
+		echo " 2) RocknRollOne"
+		echo " 3) Rounded"
+		echo " 4) SawarabiGothic"
+		echo " 5) Titre"
 		echo
 		echo -n " Indica una opción: "
 		read opcionletra
@@ -164,24 +161,16 @@ modificarSKIN()
 		esac
 	done
 	clear
-	echo -e "$blue ############################################################ $end"
-	echo -e "$blue ###          $green Comienza la modificación del skin         $blue ### $end"
-	echo -e "$blue ############################################################ $end"
+	echo " ############################################################"
+	echo " ###           Comienza la modificación del skin          ###"
+	echo " ############################################################"
 	echo
-	echo -e " Ruta  skin :$yellow $RUTASKIN $end"
-	echo -e " Tipo letra :$yellow $TIPOLETRA $end"
+	echo " Ruta  skin : $RUTASKIN"
+	echo " Tipo letra : $TIPOLETRA"
 	echo ____________________________________________________________
 	echo
-		if [ ! -f $RUTASKIN/skin.xml ]; then
-			echo -e "$red No existe el skin que has seleccionado, $end"
-			echo -e "$red comprueba la ruta y vuelve a intentarlo $end"
-			echo
-			echo -e " Pulsa intro para continuar..."
-			read CAD
-			MENU
-		fi
+		ERROR=false
 	printf "%-$(($COLUMNS-10))s"  " 1. Descargando tipo de letra elegido"
-	ERROR=false
 		if [ ! -d $RUTASKIN/fonts ]; then
 			mkdir $RUTASKIN/fonts 2>>$CARPETA_SCRIPT/dobleM.log
 			if [ $? -ne 0 ]; then
@@ -190,83 +179,85 @@ modificarSKIN()
 		fi
 		wget -O $RUTASKIN/fonts/$TIPOLETRA https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/Varios/$TIPOLETRA 2>>$CARPETA_SCRIPT/dobleM.log
 		if [ $? -eq 0 -a $ERROR = "false" ]; then
-			printf "%s$green%s$end%s\n" "[" "  OK  " "]"
+			printf "%s%s%s\n" "[" "  OK  " "]"
 		else
-			printf "%s$red%s$end%s\n" "[" "FAILED" "]"
-			echo -e "\n La descarga ha fallado.\n Por favor, inténtalo más tarde."
+			printf "%s%s%s\n" "[" "FAILED" "]"
+			echo
+			echo " La descarga ha fallado"
+			echo " Por favor, inténtalo más tarde"
 			echo
 			echo " Pulsa intro para continuar..."
 			read CAD
 			MENU
 		fi
 	printf "%-$(($COLUMNS-10))s"  " 2. Creando copia de seguridad del skin"
-		ERROR=false
 		sed -i -e '/<!-- dobleM_nuevo.ttf -->/d' -e 's/<\!-- dobleM_backup //' -e 's/ dobleM_backup -->//' $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log
 		if [ $? -ne 0 ]; then
 		ERROR=true
 		fi
-		sed -i 's|<font.*name="Regular".*/>|<!-- dobleM_backup & dobleM_backup --> \n\t& <!-- dobleM_nuevo.ttf -->|' $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log
+		sed -i 's|<font name=.*Regular.*filename=.*scale=.*100.*/>|<!-- dobleM_backup & dobleM_backup --> \n\t& <!-- dobleM_nuevo.ttf -->|' $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log		
+		if [ $? -ne 0 ]; then
+		ERROR=true
+		fi
+		sed -i 's|<font filename=.*name=.*Regular.*scale=.*100.*/>|<!-- dobleM_backup & dobleM_backup --> \n\t& <!-- dobleM_nuevo.ttf -->|' $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log		
 		if [ $? -eq 0 -a $ERROR = "false" ]; then
-			printf "%s$green%s$end%s\n" "[" "  OK  " "]"
+			printf "%s%s%s\n" "[" "  OK  " "]"
 		else
-			printf "%s$red%s$end%s\n" "[" "FAILED" "]"
+			printf "%s%s%s\n" "[" "FAILED" "]"
 		fi
 	printf "%-$(($COLUMNS-10))s"  " 3. Modificando el skin"
-		ERROR=false
-		sed -i -e "/<!-- dobleM_nuevo.ttf -->/ s|<font.*scale|<font filename=\"$RUTASKIN/fonts/$TIPOLETRA\" name=\"Regular\" scale|" $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log
+		sed -i -e "/<!-- dobleM_nuevo.ttf -->/ s|filename=\".*tf\"|filename=\"$RUTASKIN/fonts/$TIPOLETRA\"|" $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log
 		if [ $? -eq 0 ]; then
-			printf "%s$green%s$end%s\n" "[" "  OK  " "]"
+			printf "%s%s%s\n" "[" "  OK  " "]"
 		else
-			printf "%s$red%s$end%s\n" "[" "FAILED" "]"
-		fi
-	printf "\n$green%s$end\n" " ¡Proceso completado!"
-	echo
-	echo " Pulsa intro para continuar..."
-	read CAD
-	MENU
+			printf "%s%s%s\n" "[" "FAILED" "]"
+		fi	
+		printf "\n%s\n" " ¡Proceso completado!"
+		echo
+		echo " Pulsa intro para continuar..."
+		read CAD
+		MENU
 }
 
 restaurarSKIN()
 {
 	cd $CARPETA_SCRIPT
 	clear
-	echo -e "$blue ############################################################ $end"
-	echo -e "$blue ###          $green Comienza la restauración del skin         $blue ### $end"
-	echo -e "$blue ############################################################ $end"
+	echo " ############################################################"
+	echo " ###           Comienza la restauración del skin          ###"
+	echo " ############################################################"
 	echo
-	echo -e " Ruta  skin :$yellow $RUTASKIN $end"
+	echo " Ruta  skin : $RUTASKIN"
 	echo ____________________________________________________________
 	echo
-	printf "%-$(($COLUMNS-10))s"  " 1. Restaurando skin"
 		ERROR=false
+	printf "%-$(($COLUMNS-10))s"  " 1. Restaurando skin"
 		sed -i -e '/<!-- dobleM_nuevo.ttf -->/d' -e 's/<\!-- dobleM_backup //' -e 's/ dobleM_backup -->//' $RUTASKIN/skin.xml 2>>$CARPETA_SCRIPT/dobleM.log
 		if [ $? -eq 0 ]; then
-			printf "%s$green%s$end%s\n" "[" "  OK  " "]"
+			printf "%s%s%s\n" "[" "  OK  " "]"
 		else
-			printf "%s$red%s$end%s\n" "[" "FAILED" "]"
+			printf "%s%s%s\n" "[" "FAILED" "]"
 		fi
-	printf "\n$green%s$end\n" " ¡Proceso completado!"
+	printf "\n%s\n" " ¡Proceso completado!"
 	echo
 	echo " Pulsa intro para continuar..."
 	read CAD
 	MENU
 }
 
-reiniciarGUI()
+reiniciar()
 {
-	cd $CARPETA_SCRIPT
+	rm -rf $CARPETA_SCRIPT/i_dobleM*.sh
 	clear
-	echo -e "$blue Reiniciando en ...$end"
-	echo -e " 3"
+	echo " Reiniciando en ..."
+	echo " 3"
 	sleep 1
-	echo -e " 2"
+	echo " 2"
 	sleep 1
-	echo -e " 1"
+	echo " 1"
 	sleep 1
 	echo
-	rm -rf $CARPETA_SCRIPT/i_dobleM*.sh
 	reboot
-	exit
 }
 
 # MENU INSTALACION
@@ -275,19 +266,19 @@ MENU()
 while :
 do
 	clear
-	echo -e "$blue ############################################################ $end"
-	echo -e "$blue ###                    $green -= dobleM =- $end                   $blue ### $end"
-	echo -e "$blue ###             Telegram: $cyan t.me/EPG_dobleM $end             $blue ### $end"
-	echo -e "$blue ### ---------------------------------------------------- ### $end"
-	echo -e "$blue ###          $blue Instalador para sistema$yellow enigma2 $end          $blue ### $end"
-	echo -e "$blue ############################################################ $end"
+	echo " ############################################################"
+	echo " ###                     -= dobleM =-                     ###"
+	echo " ###             Telegram:  t.me/EPG_dobleM               ###"
+	echo " ### ---------------------------------------------------- ###"
+	echo " ###           Instalador para sistema enigma2            ###"
+	echo " ############################################################"
 	echo
-	echo -e " 1)$cyan Instalar$green SOURCES$end$cyan para EPG-Import$end"
-	echo -e " 2)$cyan Modificar$green SKIN$end$cyan para caracteres especiales$end"
-	echo -e " 3)$cyan Restaurar$green SKIN$end$cyan a su estado original$end"
-	echo -e " 4)$cyan Reiniciar receptor para aplicar los cambios$end"
+	echo " 1) Instalar SOURCES para EPG-Import"
+	echo " 2) Modificar SKIN para caracteres especiales"
+	echo " 3) Restaurar SKIN a su estado original"
+	echo " 4) Reiniciar receptor para aplicar los cambios"
 	echo
-    echo -e " s)$red Salir $end"
+    echo " s) Salir"
 	echo
 	echo -n " Indica una opción: "
 	read opcionmenu
@@ -295,7 +286,7 @@ do
 		1) clear && instalarEPG;;
 		2) clear && VAR="modificar" && elegirSKIN;;
 		3) clear && VAR="restaurar" && elegirSKIN;;
-		4) clear && reiniciarGUI;;
+		4) clear && reiniciar;;
 		s) clear && echo " Gracias por usar el script dobleM" && echo && rm -rf $CARPETA_SCRIPT/i_dobleM*.sh; exit;;
 		*) echo && echo " $opcionmenu es una opción inválida" && echo;
 	esac
