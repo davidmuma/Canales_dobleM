b9f53d18bb8e9934c38b6fa8d87d3dbb7cff500e
David <davidmumar@gmail.com>
2022-05-11 01:11:18 +0200

    Add files via upload

--- dobleM.sh
+++ dobleM.sh
@@ -10,14 +10,25 @@ echo Descargando última versión del instalador...
 sleep 1
 echo
 if [ -d /etc/enigma2 ]; then
-	curl -# -kO "https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMe.sh" > /dev/null 2>&1
-	wget -O i_dobleMe.sh --no-check-certificate "https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMe.sh" > /dev/null 2>&1
-	echo
-	sleep 1
-	echo Ejecutando instalador para enigma2...
-	sleep 1
-	clear
-	sh i_dobleMe.sh
+	if [ -d /home/root/.hts/tvheadend ]; then
+		curl -skO "https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMi.sh" && clear && chmod +x i_dobleMi.sh && ./i_dobleMi.sh e2
+		wget -O i_dobleMi.sh --no-check-certificate "https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMi.sh" && clear && chmod +x i_dobleMi.sh && ./i_dobleMi.sh e2
+		echo
+		sleep 1
+		echo Ejecutando instalador para tvheadend dentro de enigma2...
+		sleep 1
+		clear
+		sh i_dobleM.sh			
+	else
+		curl -# -kO "https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMe.sh" > /dev/null 2>&1
+		wget -O i_dobleMe.sh --no-check-certificate "https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleMe.sh" > /dev/null 2>&1
+		echo
+		sleep 1
+		echo Ejecutando instalador para enigma2...
+		sleep 1
+		clear
+		sh i_dobleMe.sh
+	fi
 else
 	curl -# -kO https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/files/i_dobleM.sh
 	echo
